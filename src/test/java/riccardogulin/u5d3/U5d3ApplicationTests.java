dd2cd94f6f400b5ce36080cefe07d2dd6866c10c
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-05-31 09:23:03 +0200

    Adds basic project structure

---
+++ src/test/java/riccardogulin/u5d3/U5d3ApplicationTests.java
@@ -0,0 +1,13 @@
+package riccardogulin.u5d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
